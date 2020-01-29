b0402cab86f380a3dedb80ef7cf89db8a0bf9db2
김성찬 <tjdcksdl00@naver.com>
2020-01-29 09:05:18 +0900

    commit algorithm

---
+++ Algorithm/Programmers/Programmers_구명보트.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Programmers_구명보트 {
+	public static void main(String[] args) {
+		System.out.println(solution(new int[]{70, 50, 80, 50}, 100));
+		System.out.println(solution(new int[]{70, 80, 50}, 100));
+	}
+	
+	public static int solution(int[] people, int limit) {
+        Arrays.sort(people);  
+        
+        int start = 0;
+        int end = people.length-1;
+        int count = 0;
+        
+        while(end > start) {
+            if(people[end] + people[start] <= limit) {
+                start++;
+            }
+            end--;
+            count++;
+        }
+        
+        if(start == end) {
+            count++;
+        }
+        
+        return count;
+    }
+}
